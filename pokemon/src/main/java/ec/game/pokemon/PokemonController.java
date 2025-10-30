cfa5048bf887158f06e8543e2da8d440454a3359
Walther Gaibor <wgaibor@telconet.ec>
2025-10-29 21:39:32 -0500

    Inicio de la creación de la app pokemon

--- pokemon/src/main/java/ec/game/pokemon/PokemonController.java
+++ pokemon/src/main/java/ec/game/pokemon/PokemonController.java
@@ -3,7 +3,7 @@ package ec.game.pokemon;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class PokemonController {
 
     @FXML
     private void switchToSecondary() throws IOException {
--- pokemon/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module ec.game.pokemon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens ec.game.pokemon to javafx.fxml;
-    exports ec.game.pokemon;
-}
